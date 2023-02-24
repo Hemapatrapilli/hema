db7011ea5370c533638c0f66ded76db3adf500dc
Hemapatrapilli <125134924+Hemapatrapilli@users.noreply.github.com>
2023-02-24 21:21:28 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int i,n,fact=1;
+	scanf("%d",&n);
+	i=1;
+	while(i<=n)
+	{
+	
+	fact=fact*i;
+	
+	i++;
+}
+printf("%d",fact);
+}
---
+++ rev.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int i,n,j;
+	scanf("%d",&n);
+	for(i=1,j=n;i<=n,j>=1;i++,j--)
+	{
+	
+	printf("%d %d\n",i,j);
+	
+}}
